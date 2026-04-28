14c3f2784ca5db9426a25b58b312749f515d6628
davidbuyck <47149695+davidbuyck@users.noreply.github.com>
2026-04-28 17:06:49 -0500

    Added test mesh

---
+++ projects/my-webgl-build/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-VinettesUnity-0.1.0";
+const contentToCache = [
+    "Build/UnityBuilds.loader.js",
+    "Build/UnityBuilds.framework.js",
+    "Build/UnityBuilds.data",
+    "Build/UnityBuilds.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
